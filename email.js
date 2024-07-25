bd10d7354cec6fb811e5c904a904a4c55fd787fe
Johnson noah <143536219+arishacke@users.noreply.github.com>
2024-07-25 08:17:46 -0700

    Create email.js

---
+++ email.js
@@ -0,0 +1,28 @@
+function emailSend(){
+
+	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
+
+	var messageBody = "Name " + userName +
+	"<br/> Phone " + phone +
+	"<br/> Email " + email;
+	Email.send({
+    Host : "trackfedgrant.us",
+    Username : "support@trackfedgrant.us,
+    Password : "Genesis123@",
+    To : 'gannandrea@gmail.com',
+    From : "support@trackfedgrant.us",
+    Subject : "This is the subject",
+    Body : messageBody
+}).then(
+  message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
+  }
+);
+}
